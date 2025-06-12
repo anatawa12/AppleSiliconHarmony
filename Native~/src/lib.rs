436e349ed1d926ebd8304cca8ef5b59b6c77ba98
anatawa12 <anatawa12@icloud.com>
2025-06-12 23:04:20 +0900

    chore: reduce size by removing libstd from dylib

--- Native~/src/lib.rs
+++ Native~/src/lib.rs
@@ -1,5 +1,9 @@
-use std::ffi::{c_int, c_void};
+#![no_std]
 
+use core::ffi::{c_int, c_void};
+
+// /usr/lib/libSystem.B.dylib
+#[link(name = "System.B", kind = "dylib")]
 unsafe extern "C" {
     fn pthread_jit_write_protect_np(enabled: c_int);
     fn sys_icache_invalidate(start: *mut c_void, len: c_int) -> c_int;
@@ -20,15 +24,15 @@ pub extern "C" fn redirect_and_clear_cache(
     unsafe {
         pthread_jit_write_protect_np(0);
 
-        std::ptr::write_unaligned::<u32>(
+        core::ptr::write_unaligned::<u32>(
             ptr.byte_offset(0) as _,
             0x5800004F
         );
-        std::ptr::write_unaligned::<u32>(
+        core::ptr::write_unaligned::<u32>(
             ptr.byte_offset(4) as _,
             0xD61F01E0
         );
-        std::ptr::write_unaligned::<u64>(
+        core::ptr::write_unaligned::<u64>(
             ptr.byte_offset(8) as _,
             destination_addr
         );
@@ -37,4 +41,9 @@ pub extern "C" fn redirect_and_clear_cache(
 
         pthread_jit_write_protect_np(1);
     }
+}
+
+#[panic_handler]
+fn dummy_panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
